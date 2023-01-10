ac8ed056fd61a3400b6efb8d49e98daf846ea474
Jasmine Ulloa <ulloajazz6@gmail.com>
2023-01-09 19:42:15 -0800

    saved the users input into local storage

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,19 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var saveBtn = $('.saveBtn');
+
 $(function () {
+
+  saveBtn.on("click", function(){
+    var time = $(this).parent().attr("id");
+    var value = $(this).siblings(".description").val();
+
+    localStorage.setItem(time, value);
+
+  });
+
+  $("#9 .description").val(localStorage.getItem('9'));
   
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
