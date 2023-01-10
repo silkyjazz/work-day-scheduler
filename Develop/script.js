9d05aa1bc11e264a5daaf54991a274e4373a174c
Jasmine Ulloa <ulloajazz6@gmail.com>
2023-01-09 19:28:22 -0800

    added hours 12-5pm

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,9 @@
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,3 +23,4 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+    
